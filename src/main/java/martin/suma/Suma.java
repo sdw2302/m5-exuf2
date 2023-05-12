72d28dea47ddb0b0ca731d3af645a422ac5e48ba
sdw2302 <shadow2302ezg@gmail.com>
2023-05-12 10:23:46 +0200

    metode sumaValorAbsolut creat

--- src/main/java/martin/suma/Suma.java
+++ src/main/java/martin/suma/Suma.java
@@ -3,4 +3,17 @@ package martin.suma;
 public class Suma {
     private int num1;
     private int num2;
+
+    public int sumaValorAbsolut() {
+        int x, y;
+        if (num1 < 0)
+            x = -num1;
+        else
+            x = num1;
+        if (num2 < 0)
+            y = -num2;
+        else
+            y = num2;
+        return x + y;
+    }
 }
