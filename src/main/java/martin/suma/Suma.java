4b2e413c2056bbaaca4728c641e531da0f255fd7
sdw2302 <shadow2302ezg@gmail.com>
2023-05-12 10:31:14 +0200

    constructor i test1 creat

--- src/main/java/martin/suma/Suma.java
+++ src/main/java/martin/suma/Suma.java
@@ -4,6 +4,11 @@ public class Suma {
     private int num1;
     private int num2;
 
+    public Suma(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
     public int sumar() {
         return num1 + num2;
     }
--- src/main/test/java/martin/suma/SumaTest.java
+++ src/main/test/java/martin/suma/SumaTest.java
@@ -1,5 +1,15 @@
 package martin.suma;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Suma {
-    
+    @Test
+    public void sumarTest() {
+        int x = 5, y = -3;
+        int exp = 2;
+        Suma test = new Suma(x, y);
+        res = test.sumar();
+        assertEquals(exp, res);
+    }
 }
